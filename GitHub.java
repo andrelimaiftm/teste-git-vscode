56228f168f7fdccf63c7f12c068477d4acf73926
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-23 15:04:25 -0300

    alteração da classe GitHub

--- GitHub.java
+++ GitHub.java
@@ -1,6 +1,7 @@
 public class GitHub {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.println("Seja bem vindo a aula de lp2");
+        System.out.println("teste commit");
         
     }
     
